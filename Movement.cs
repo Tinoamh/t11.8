f1e173e6ba10d264e3c9537173b2a47a061a8eba
Tinoamh <tinahmohammadi82@gmail.com>
2025-11-02 20:53:11 +0330

    Revert "second"
    This reverts commit 0e0929a0df77627e101db91e0b8a8f8b3ee3ef88.

--- Movement.cs
+++ Movement.cs
@@ -9,10 +9,8 @@ public class Movement : MonoBehaviour
 {
     [SerializeField]private float speed;
     private Rigidbody2D body;
-    private float t1;
-    private void Awake()
-    {
-        body = GetComponent<Rigidbody2D>();
+    private void Awake(){
+        body=GetComponent<Rigidbody2D>();
     }
     private void Update(){
         body.velocity=new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
