a0f643ce7e0334f893cb1a1a91ba5b94249e01ca
Tinoamh <tinahmohammadi82@gmail.com>
2025-11-02 20:30:46 +0330

    edit

---
+++ Movement.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    private Rigidbody2D body;
+    private void Awake(){
+        body=GetComponent<Rigidbody2D>();
+    }
+    private void Update(){
+        body.velocity=new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
+        if(Input.GetKey(KeyCode.Space))
+            body.velocity=new Vector2(body.velocity.x,speed);
+    }
+}
